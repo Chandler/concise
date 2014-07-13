2a9638ae7bdf01bae1b5b11f4817cbe241074827
Helen Zeng <helen.w.zeng@gmail.com>
2014-07-12 23:45:44 -0700

    cleaned up stream observer

--- concise.js
+++ concise.js
@@ -7,34 +7,33 @@ $( document ).ready(function() {
 
   var records = [];
 
+  //checks & saves changes in tweet
   var mutationCallback = function(mutations) {
     mutations.forEach(function(mutation) {
       var text = $(mutation.target).text();
-      records.push({time: Date.now(), text: text});
-      console.log(text);
+      if(text !== "Compose new Tweet..."){
+        records.push({time: Date.now(), text: text});
+        console.log(text);
+      }
     });
+  }
+  //checks stream for newly posted tweet
+  var streamMutationCallback = function(){
+      if($(".my-tweet .tweet-text").first().text() == records[records.length-1].text){
+        var tweetID = $(".my-tweet").first().attr("data-tweet-id").toString();
+        chrome.storage.sync.set({ tweetID : "test"}, function() {
+          console.log(tweetID + " saved");
+          records = [];
+        });
+      }
   }
 
   var observer = new MutationObserver(mutationCallback);
-
   observer.observe(target, config);
 
   $(".tweet-action").click(function(){
-    observer.disconnect();
-    debugger
+    var streamObserver = new MutationObserver(streamMutationCallback);
     var stream = $("#stream-items-id")[0];
-    var streamObserver = new MutationObserver(function(){
-    	if($(".my-tweet .tweet-text").first().text() == records[records.length-1].text){
-    		
-        streamObserver.disconnect();
-        
-        var tweetID = $(".my-tweet").first().attr("data-tweet-id").toString();
-        
-        chrome.storage.sync.set({ tweetID: records[records.length-1]}, function() {
-          console.log(tweetID + " saved");
-        });
-    	}
-    });
     streamObserver.observe(stream, config);
   });
 });
